611c15925c864333aa39b8b11295236f300bf33a
Felipe veiga <fcosta.veiga@gmail.com>
2025-08-28 18:34:25 -0300

    teste: implementa teste unitario do GetUser do service

--- service/user_test.go
+++ service/user_test.go
@@ -4,6 +4,7 @@ import (
 	"errors"
 	"testing"
 
+	"github.com/felipecveiga/crud-puro-go/errs"
 	"github.com/felipecveiga/crud-puro-go/model"
 	"github.com/felipecveiga/crud-puro-go/repository"
 	"github.com/stretchr/testify/assert"
@@ -60,7 +61,7 @@ func TestCreateUser_WhenCreateAccount_ReturnErrorNameEmpty(t *testing.T) {
 	err := service.CreateUser(payload)
 
 	assert.Error(t, err)
-	assert.EqualError(t, err, "erro ao criar conta, preenchimento obrigatório do nome, email e telefone")
+	assert.EqualError(t, err, "missing required fields: name, email, and phone number")
 }
 
 func TestCreateUser_WhenCreateAccount_ReturnErrorCreate(t *testing.T) {
@@ -90,4 +91,66 @@ func TestCreateUser_WhenCreateAccount_ReturnErrorCreate(t *testing.T) {
 
 	assert.Error(t, err)
 	assert.EqualError(t, err, "some error")
+}
+
+func TestGetUser_WhenGetUser_ReturnSucess(t *testing.T) {
+	ctrl := gomock.NewController(t)
+	mockRepo := repository.NewMockRepository(ctrl)
+	service := NewUserService(mockRepo)
+
+	user := &model.User{
+		Name:  "Felipe",
+		Email: "felipe@gmail.com",
+		Sexo:  "M",
+		Age:   31,
+		Phone: 21212121,
+		Residence: model.Residence{
+			Street:  "rua A",
+			City:    "rio de janeiro",
+			Country: "Brasil",
+			Number:  27,
+		},
+	}
+
+	id := "68a8e66a5a3b238655f42f4"
+	mockRepo.EXPECT().
+		FindByID(id).
+		Return(user, nil)
+
+	result, err := service.GetUser(id)
+
+	assert.NoError(t, err)
+	assert.Equal(t, user, result)
+}
+
+func TestGetUser_WhenGetUser_ReturnErrorUserNotFound(t *testing.T) {
+	ctrl := gomock.NewController(t)
+	mockRepo := repository.NewMockRepository(ctrl)
+	service := NewUserService(mockRepo)
+
+	id := "68a8e66a5a3b238655f42f4"
+	mockRepo.EXPECT().
+		FindByID(id).
+		Return(nil, errs.ErrUserNotFound)
+
+	_, err := service.GetUser(id)
+
+	assert.Error(t, err)
+	assert.EqualError(t, err, "user not found")
+}
+
+func TestGetUser_WhenGetUser_ReturnError(t *testing.T) {
+	ctrl := gomock.NewController(t)
+	mockRepo := repository.NewMockRepository(ctrl)
+	service := NewUserService(mockRepo)
+
+	id := "68a8e66a5a3b238655f42f4"
+	mockRepo.EXPECT().
+		FindByID(id).
+		Return(nil, errs.ErrUserSearchFailed)
+
+	_, err := service.GetUser(id)
+
+	assert.Error(t, err)
+	assert.EqualError(t, err, "failed to search for user")
 }
